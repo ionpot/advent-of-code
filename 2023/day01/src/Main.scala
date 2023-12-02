37b3879cc50bc94c0c9816ff55e321933d330ddd
Emir Kotan <emirkotan@gmail.com>
2023-12-02 20:56:13 +0000

    2023 day 1 part 2

--- 2023/day01/src/Main.scala
+++ 2023/day01/src/Main.scala
@@ -13,10 +13,34 @@ def calibrate(input: Seq[String]): Int =
       case None => 0
   numbers.sum
 
-val digit = "[0-9]".r
+val digits = Seq(
+  "one",
+  "two",
+  "three",
+  "four",
+  "five",
+  "six",
+  "seven",
+  "eight",
+  "nine",
+)
+
+def convert(digit: String): String =
+  val i = digits.indexOf(digit) + 1
+  if (i == 0) digit else i.toString()
+
+val digit = Seq("[0-9]").concat(digits).mkString("|").r
+
+def findLastIn(input: String, last: String = ""): String =
+  digit.findFirstMatchIn(input) match
+    case Some(value) =>
+      val sub = input.substring(value.start + 1)
+      findLastIn(sub, value.matched)
+    case None => last
 
 def findDigits(line: String): String =
-  val found = digit.findAllMatchIn(line).toSeq
-  (found.headOption, found.lastOption) match
-    case (Some(head), Some(last)) => head.matched + last.matched
+  digit.findFirstIn(line) match
+    case Some(found) =>
+      val last = findLastIn(line)
+      convert(found) + convert(last)
     case _ => ""
